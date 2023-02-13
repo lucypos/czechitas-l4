0d91d45882c29b82c19b51a7db9af6b0c692c6d2
Lucie Pospisilova <pospisilova.lucy@gmail.com>
2023-02-13 12:45:33 +0100

    DÚ

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-// tady je místo pro náš program
+
+//1. úkol
+let title = 'Nejhorší člověk na světě'
+//title.length
+
+title.toUpperCase()
 
 
+title.slice(0, 5)
+
+title.slice(19, 24)
+
+//2. úkol
+let email = prompt('Zadej svůj e-mail')
+let atIndex = email.indexOf('@')
 
 
+const parsedEmail = {
+	username: email.slice(0, atIndex),
+	domain: email.slice(atIndex + 1),
+}
+document.body.innerHTML += '<p>Uživatelské jméno: ' + parsedEmail.username + '</p>'
+document.body.innerHTML += '<p>Doména: ' + parsedEmail.domain + '</p>'
